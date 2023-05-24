6b1c885091abe2977cab746f97db730d51bb7994
AxOn7065 <88163103+AxOn7065@users.noreply.github.com>
2023-05-24 10:55:10 +0300

    Update code.php

--- code.php
+++ code.php
@@ -1,10 +1,10 @@
 
 <?php
 return array(
-  'udid' => '1',
-  'udid2' => '12',
-  'udid3' => '123',
-  // add more UDID-password pairs as needed
+ 
+
+
+// add more UDID-password pairs as needed
 );
 ?>
 
